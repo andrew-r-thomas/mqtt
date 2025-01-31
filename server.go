6755c4b767566994437e1fa0092281e58f81ff06
Andrew Thomas <andrew@andrewrthomas.com>
2025-01-30 16:10:52 -0800

    clean up

--- server.go
+++ server.go
@@ -1,7 +1,6 @@
 package mqtt
 
 import (
-	"context"
 	"log"
 	"net"
 	"strings"
@@ -103,138 +102,113 @@ func handleClient(
 		bp,
 	)
 
-	sender := make(chan []byte, 10)
-	addCliChan <- AddCliMsg{ClientId: connect.Id, Sender: sender}
-
 	readChan := make(chan Packet, 10)
-	ctx, cancel := context.WithCancel(context.Background())
-	go readPump(conn, bp, readChan, ctx)
+	writeChan := make(chan []byte, 10)
+	addCliChan <- AddCliMsg{ClientId: connect.Id, Sender: writeChan}
+	go readPump(conn, bp, readChan)
+	go writePump(conn, bp, writeChan)
 
-	for {
-		select {
-		case pub := <-sender:
-			conn.Write(pub)
-			bp.ReturnBuf(pub)
-		case packet := <-readChan:
-			offset := len(packet.buf) - int(packet.fh.RemLen)
-			switch packet.fh.Pt {
-			case packets.PUBLISH:
-				pl.Properties.Zero()
-				packets.DecodePublish(
-					&packet.fh,
-					&pl.Publish,
-					&pl.Properties,
-					packet.buf[offset:],
-				)
-				pubChan <- PubMsg{
-					Topic: pl.Publish.Topic,
-					Msg:   packet.buf,
-				}
-			case packets.PINGREQ:
-				clear(packet.buf)
-				packet.buf[0] = 0b11010000
-				packet.buf[1] = 0
-				_, err := conn.Write(packet.buf[:2])
-				if err != nil {
-					log.Fatalf("ahhh! %v\n", err)
-				}
-				bp.ReturnBuf(packet.buf)
-			case packets.SUBSCRIBE:
-				pl.Properties.Zero()
-				pl.Subscribe.Zero()
-				packets.DecodeSubscribe(
-					&pl.Subscribe,
-					&pl.Properties,
-					packet.buf[offset:],
-				)
-
-				pl.Suback.Zero()
-				tfs := [][]string{}
-				for _, filter := range pl.Subscribe.TopicFilters {
-					// TODO: validate filter in here
-					pl.Suback.ReasonCodes = append(
-						pl.Suback.ReasonCodes, 0,
-					)
-					tfs = append(
-						tfs,
-						strings.Split(
-							filter.Filter,
-							"/",
-						),
-					)
-				}
-				subChan <- SubMsg{
-					ClientId:     connect.Id,
-					TopicFilters: tfs,
-				}
-
-				pl.Suback.PacketId = pl.Subscribe.PackedId
-				pl.Properties.Zero()
-				clear(packet.buf)
-				scratch := bp.GetBuf()
-				i := packets.EncodeSuback(
-					&pl.Suback,
-					&pl.Properties,
-					packet.buf,
-					scratch,
-				)
-				_, err := conn.Write(packet.buf[:i])
-				if err != nil {
-					log.Fatalf(
-						"error writing suback: %v\n",
-						err,
-					)
-				}
-				bp.ReturnBuf(scratch)
-				bp.ReturnBuf(packet.buf)
-			case packets.UNSUBSCRIBE:
-				for _, b := range packet.buf {
-					log.Printf("%08b\n", b)
-				}
-
-				pl.Properties.Zero()
-				pl.Unsubscribe.Zero()
-				packets.DecodeUnsubscribe(
-					&pl.Unsubscribe,
-					&pl.Properties,
-					packet.buf[offset:],
-				)
-
-				pl.Unsuback.Zero()
-				tfs := [][]string{}
-				for _, filter := range pl.Unsubscribe.TopciFilters {
-					pl.Unsuback.ReasonCodes = append(pl.Unsuback.ReasonCodes, 0)
-					tfs = append(tfs, strings.Split(filter, "/"))
-				}
-				unSubChan <- UnSubMsg{
-					ClientId:     connect.Id,
-					TopicFilters: tfs,
-				}
-
-				pl.Unsuback.PacketId = pl.Unsubscribe.PacketId
-				pl.Properties.Zero()
-				clear(packet.buf)
-				scratch := bp.GetBuf()
-				i := packets.EncodeUnsuback(&pl.Unsuback, &pl.Properties, packet.buf, scratch)
-				_, err := conn.Write(packet.buf[:i])
-				if err != nil {
-					log.Fatalf(
-						"error writing unsuback: %v\n",
-						err,
-					)
-				}
-				bp.ReturnBuf(scratch)
-				bp.ReturnBuf(packet.buf)
-			case packets.DISCONNECT:
-				cancel()
-				remCliChan <- RemCliMsg{
-					ClientId: connect.Id,
-				}
-				return
-			default:
-				log.Printf("bad packet\n")
-				bp.ReturnBuf(packet.buf)
+	for packet := range readChan {
+		offset := len(packet.buf) - int(packet.fh.RemLen)
+		switch packet.fh.Pt {
+		case packets.PUBLISH:
+			pl.Properties.Zero()
+			packets.DecodePublish(
+				&packet.fh,
+				&pl.Publish,
+				&pl.Properties,
+				packet.buf[offset:],
+			)
+			pubChan <- PubMsg{
+				Topic: pl.Publish.Topic,
+				Msg:   packet.buf,
 			}
+		case packets.PINGREQ:
+			clear(packet.buf)
+			packet.buf[0] = 0b11010000
+			packet.buf[1] = 0
+			writeChan <- packet.buf[:2]
+		case packets.SUBSCRIBE:
+			pl.Properties.Zero()
+			pl.Subscribe.Zero()
+			packets.DecodeSubscribe(
+				&pl.Subscribe,
+				&pl.Properties,
+				packet.buf[offset:],
+			)
+
+			pl.Suback.Zero()
+			tfs := [][]string{}
+			for _, filter := range pl.Subscribe.TopicFilters {
+				// TODO: validate filter in here
+				pl.Suback.ReasonCodes = append(
+					pl.Suback.ReasonCodes, 0,
+				)
+				tfs = append(
+					tfs,
+					strings.Split(
+						filter.Filter,
+						"/",
+					),
+				)
+			}
+			subChan <- SubMsg{
+				ClientId:     connect.Id,
+				TopicFilters: tfs,
+			}
+
+			pl.Suback.PacketId = pl.Subscribe.PackedId
+			pl.Properties.Zero()
+			clear(packet.buf)
+			scratch := bp.GetBuf()
+			i := packets.EncodeSuback(
+				&pl.Suback,
+				&pl.Properties,
+				packet.buf,
+				scratch,
+			)
+			bp.ReturnBuf(scratch)
+			writeChan <- packet.buf[:i]
+		case packets.UNSUBSCRIBE:
+			for _, b := range packet.buf {
+				log.Printf("%08b\n", b)
+			}
+
+			pl.Properties.Zero()
+			pl.Unsubscribe.Zero()
+			packets.DecodeUnsubscribe(
+				&pl.Unsubscribe,
+				&pl.Properties,
+				packet.buf[offset:],
+			)
+
+			pl.Unsuback.Zero()
+			tfs := [][]string{}
+			for _, filter := range pl.Unsubscribe.TopciFilters {
+				pl.Unsuback.ReasonCodes = append(pl.Unsuback.ReasonCodes, 0)
+				tfs = append(tfs, strings.Split(filter, "/"))
+			}
+			unSubChan <- UnSubMsg{
+				ClientId:     connect.Id,
+				TopicFilters: tfs,
+			}
+
+			pl.Unsuback.PacketId = pl.Unsubscribe.PacketId
+			pl.Properties.Zero()
+			clear(packet.buf)
+			scratch := bp.GetBuf()
+			i := packets.EncodeUnsuback(&pl.Unsuback, &pl.Properties, packet.buf, scratch)
+			bp.ReturnBuf(scratch)
+			writeChan <- packet.buf[:i]
+		case packets.DISCONNECT:
+			remCliChan <- RemCliMsg{
+				ClientId: connect.Id,
+			}
+			close(writeChan)
+			return
+		default:
+			log.Printf("bad packet\n")
+			bp.ReturnBuf(packet.buf)
 		}
 	}
 }
@@ -306,40 +280,51 @@ func readPump(
 	conn net.Conn,
 	bp *BufPool,
 	sender chan<- Packet,
-	ctx context.Context,
 ) {
 	fh := packets.FixedHeader{}
 
 	for {
-		select {
-		case <-ctx.Done():
-			return
-		default:
-			fh.Zero()
-			buf := bp.GetBuf()
+		fh.Zero()
+		buf := bp.GetBuf()
 
-			n, err := conn.Read(buf)
-			if err != nil {
-				log.Fatalf("error reading from conn! %v\n", err)
-			}
-
-			offset, err := packets.DecodeFixedHeader(&fh, buf)
-
-			for n < int(fh.RemLen)+offset {
-				// didn't read enough
-				b := bp.GetBuf()
-				defer bp.ReturnBuf(b)
-
-				nn, err := conn.Read(b)
-				if err != nil {
-					log.Fatalf("error reading from conn: %v\n", err)
-				}
-
-				n += nn
-				buf = append(buf, b...)
-			}
-
-			sender <- Packet{fh: fh, buf: buf[:n]}
+		n, err := conn.Read(buf)
+		if err != nil {
+			log.Fatalf("error reading from conn! %v\n", err)
 		}
+
+		offset, err := packets.DecodeFixedHeader(&fh, buf)
+
+		for n < int(fh.RemLen)+offset {
+			// didn't read enough
+			b := bp.GetBuf()
+			defer bp.ReturnBuf(b)
+
+			nn, err := conn.Read(b)
+			if err != nil {
+				log.Fatalf("error reading from conn: %v\n", err)
+			}
+
+			n += nn
+			buf = append(buf, b...)
+		}
+
+		sender <- Packet{fh: fh, buf: buf[:n]}
+
+		if fh.Pt == packets.DISCONNECT {
+			return
+		}
+	}
+}
+
+func writePump(conn net.Conn, bp *BufPool, c <-chan []byte) {
+	for buf := range c {
+		n, err := conn.Write(buf)
+		if err != nil {
+			log.Fatalf("error writting to conn: %v\n", err)
+		}
+		if n != len(buf) {
+			log.Fatalf("did not write full buf\n")
+		}
+		bp.ReturnBuf(buf)
 	}
 }
